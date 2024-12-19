ca2e1e2645e3e77c18ca8f8b968020536c7024cc
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2024-12-19 12:42:12 +0200

    fix: solve next/image hosting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["inventory-backend-sqbj.onrender.com"],
+    domains: ["inventory-backend-sqbj.onrender.com", "localhost"],
   },
 };
 
